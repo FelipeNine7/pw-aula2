98b6c8114674053ecefd0f4f54d357e9ec9e87cc
FelipeNine7 <diasandis@gmail.com>
2019-08-07 22:27:32 -0300

    Exercicio2 doador de sangue

---
+++ dist/exercicio2.js
@@ -0,0 +1,8 @@
+var idade = 68;
+console.log(idade);
+if (idade >= 18 && idade <= 67) {
+    console.log("SIM");
+}
+else {
+    console.log("NÃO");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,10 @@
+let idade: number = 68
+console.log(idade)
+if(idade>= 18 && idade <=67)
+{
+console.log("SIM")
+}
+else
+{
+    console.log("NÃO")
+}

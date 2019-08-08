7b05b5ff6718801d4088fe64d615190606223321
FelipeNine7 <diasandis@gmail.com>
2019-08-07 22:01:40 -0300

    Exercicio aula-2 Aprovado ou Reprovado

---
+++ dist/exercicio.js
@@ -0,0 +1,12 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var Md;
+Md = (nota1 + nota2 + nota3) / 3;
+console.log("A Média é: " + Md);
+if (Md >= 7) {
+    console.log("Aprovado");
+}
+else {
+    console.log("Reprovado");
+}
---
+++ exercicio.ts
@@ -0,0 +1,17 @@
+let nota1: number = 10
+let nota2: number = 9
+let nota3: number = 5
+let Md
+Md=(nota1+nota2+nota3)/3
+console.log("A Média é: " + Md)
+if(Md>=7)
+{
+console.log("Aprovado")
+}
+else
+{
+    console.log("Reprovado")
+}
+
+
+
